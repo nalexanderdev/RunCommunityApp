6e86b73f4eff12bfabb5aff8594073bf8ddc1c4a
nalexanderdev <nalexanderdev@gmail.com>
2017-06-13 19:32:43 +0100

    Initial commit

---
+++ app/src/main/java/com/nalexanderdev/runcommunity/TextView.java
@@ -0,0 +1,11 @@
+package com.nalexanderdev.runcommunity;
+
+/**
+ * Created by user on 13-Jun-17.
+ */
+
+class TextView {
+    public void setOnClickListener(MainActivity mainActivity) {
+
+    }
+}
