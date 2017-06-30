dfef52a414433a4e20be698f9b66088c75298b9f
nalexanderdev <nalexanderdev@gmail.com>
2017-06-30 10:52:03 +0100

    Upload images to firebase

--- app/src/main/java/com/nalexanderdev/runcommunity/activities/ProfileActivity.java
+++ app/src/main/java/com/nalexanderdev/runcommunity/activities/ProfileActivity.java
@@ -1,20 +1,27 @@
 package com.nalexanderdev.runcommunity.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.nalexanderdev.runcommunity.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.nalexanderdev.runcommunity.R;
 
 public class ProfileActivity extends BaseActivity {
 
@@ -25,10 +32,18 @@ public class ProfileActivity extends BaseActivity {
     FirebaseAuth.AuthStateListener listener;
     FirebaseUser user;
 
+    private Button mSelectImage;
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT = 2;
+    private ProgressDialog mProgressDialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
 
         emailF = (EditText) findViewById(R.id.emailField);
         nameF = (EditText) findViewById(R.id.nameField);
@@ -39,6 +54,21 @@ public class ProfileActivity extends BaseActivity {
         nameF.setEnabled(false);
         updateBtn.setVisibility(View.INVISIBLE);
         cancelBtn.setVisibility(View.INVISIBLE);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mSelectImage = (Button) findViewById(R.id.selectImage);
+        mProgressDialog = new ProgressDialog(this);
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                intent.setType("image/*");
+
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
 
         auth = FirebaseAuth.getInstance();
         listener = new FirebaseAuth.AuthStateListener() {
@@ -160,4 +190,31 @@ public class ProfileActivity extends BaseActivity {
             auth.removeAuthStateListener(listener);
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+
+            mProgressDialog.setMessage("Uploading...");
+            mProgressDialog.show();
+
+            Uri uri = data.getData();
+
+            StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(ProfileActivity.this, "Upload done", Toast.LENGTH_LONG).show();
+                    mProgressDialog.dismiss();
+                }
+
+            });
+        }
+    }
 }
+
+
+
