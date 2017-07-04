a6d02261cd0f6052a673a4cbe6771406e3e833c9
nalexanderdev <nalexanderdev@gmail.com>
2017-07-04 22:37:31 +0100

    add longClick for delete button

--- app/src/main/java/com/nalexanderdev/runcommunity/activities/PostActivity.java
+++ app/src/main/java/com/nalexanderdev/runcommunity/activities/PostActivity.java
@@ -1,9 +1,9 @@
 package com.nalexanderdev.runcommunity.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +11,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import android.widget.Toast;
-import com.nalexanderdev.runcommunity.R;
-import com.nalexanderdev.runcommunity.adapters.PostListViewAdapter;
-import com.nalexanderdev.runcommunity.fragmenst.CreatePostDialog;
-import com.nalexanderdev.runcommunity.fragmenst.DeletePostDialog;
-import com.nalexanderdev.runcommunity.models.Post;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -25,6 +19,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.nalexanderdev.runcommunity.R;
+import com.nalexanderdev.runcommunity.adapters.PostListViewAdapter;
+import com.nalexanderdev.runcommunity.fragmenst.CreatePostDialog;
+import com.nalexanderdev.runcommunity.fragmenst.DeletePostDialog;
+import com.nalexanderdev.runcommunity.models.Post;
 
 import java.util.ArrayList;
 import java.util.List;
